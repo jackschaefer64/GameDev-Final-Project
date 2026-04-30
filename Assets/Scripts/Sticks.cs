35c404b19745be2b7b5859f0670931534feb85f7
Paul M <pmaitner2@gmail.com>
2026-04-30 15:07:51 -0400

    Classwork

---
+++ Assets/Scripts/Sticks.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StickCollection : MonoBehaviour
+{
+
+    private int _stickcount = 0;
+    public TextMeshProUGUI _stickText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Stick"))
+        {
+            _stickcount += 1;
+            Destroy(other.gameObject);
+            _stickText.text = "Sticks: " + _stickcount.ToString();
+        }
+    }
+
+}
